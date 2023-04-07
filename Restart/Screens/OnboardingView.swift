6e70ed5e8c9016d04fd03279bf1f5ef6590fecae
Mahesh Yakami <yakami.mahesh@gmail.com>
2023-04-07 17:55:04 +0545

    haptic feedback added to onboarding button drag gesture

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -17,6 +17,8 @@ struct OnboardingView: View {
     @State private var indicatorOpacity: Double = 1.0
     @State private var textTitle: String = "Share."
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     // MARK: Body
     
     var body: some View {
@@ -151,10 +153,12 @@ struct OnboardingView: View {
                                 }).onEnded({ _ in
                                     withAnimation(Animation.easeOut(duration: 0.3)) {
                                         if buttonOffset > buttonWidth / 2 {
+                                            hapticFeedback.notificationOccurred(.success)
                                             playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingViewActive = false
                                         } else {
+                                            hapticFeedback.notificationOccurred(.warning)
                                             buttonOffset = 0
                                         }
                                     }
