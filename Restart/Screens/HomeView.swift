f654bfd320464af78de41023fd477375952391da
Mahesh Yakami <yakami.mahesh@gmail.com>
2023-04-09 13:56:18 +0545

    homeview background color set and text color update

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -14,54 +14,57 @@ struct HomeView: View {
     
     // MARK: Body
     var body: some View {
-        VStack(spacing: 20) {
-            Spacer()
-            // MARK: Header
+        ZStack {
+            Color(.white)
+            VStack(spacing: 20) {
+                Spacer()
+                // MARK: Header
 
-            ZStack {
-                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
-                Image("character-2")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                    .offset(y: isAnimating ? 35: -35)
-                    .animation(.easeInOut(duration: 3).repeatForever(), value: isAnimating)
-            }
-            
-            
-            // MARK: Center
-            Text("The time that leads to mastery is dependent on the intensity of our color")
-                .font(.title3)
-                .foregroundColor(.secondary)
-                .fontWeight(.light)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            
-            // MARK: Footer
-            Spacer()
-            
-            Button {
-                withAnimation {
-                    playSound(sound: "success", type: "m4a")
-                    isOnboardingViewActive = true
+                ZStack {
+                    CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                    Image("character-2")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .offset(y: isAnimating ? 35: -35)
+                        .animation(.easeInOut(duration: 3).repeatForever(), value: isAnimating)
                 }
                 
-            } label: {
-                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
-                Text("Restart")
-                    .font(.system(.title3, design: .rounded))
-                    .fontWeight(.bold)
+                
+                // MARK: Center
+                Text("The time that leads to mastery is dependent on the intensity of our color")
+                    .font(.title3)
+                    .foregroundColor(.black)
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                
+                // MARK: Footer
+                Spacer()
+                
+                Button {
+                    withAnimation {
+                        playSound(sound: "success", type: "m4a")
+                        isOnboardingViewActive = true
+                    }
+                    
+                } label: {
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    Text("Restart")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+                .padding()
             }
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .controlSize(.large)
-            .padding()
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    isAnimating = true
+                })
         }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-                isAnimating = true
-            })
         }
     }
 }
